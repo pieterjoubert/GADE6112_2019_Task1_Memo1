7579d4b56d7d15238353b63737cc126b269fa535
pieterjoubert <pieter.joubert@gmail.com>
2019-10-04 14:05:34 +0200

    Added Building class.

---
+++ GADE6112_2019_Task1_Memo1/Building.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_2019_Task1_Memo1
+{
+    class Building
+    {
+    }
+}
