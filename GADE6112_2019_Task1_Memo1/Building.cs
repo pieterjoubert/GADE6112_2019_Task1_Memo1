7ecd94f2ab9c15bf8abd06669518f301eb79826c
pieterjoubert <pieter.joubert@gmail.com>
2019-10-04 14:09:15 +0200

    Add protected fields to Building class

--- GADE6112_2019_Task1_Memo1/Building.cs
+++ GADE6112_2019_Task1_Memo1/Building.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace GADE6112_2019_Task1_Memo1
 {
-    class Building
+    public abstract class Building
     {
+        protected int xPos;
+        protected int yPos;
+        protected int hp;
+        protected int maxHP;
+        protected int team;
+        protected string symbol;
     }
 }
